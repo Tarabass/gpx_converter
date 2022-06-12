aa6f88fe5c1677d6f95b6df352bee39e8ca35ab9
Tarabass <p.rietveld@live.com>
2022-06-12 22:44:44 +0200

    Make ttl of converted files configurable

--- services/gpxconverter.js
+++ services/gpxconverter.js
@@ -42,7 +42,7 @@ module.exports = function() {
                 }
 
                 //3600000
-                if (fileOlderThen(stats, 60000)) {
+                if (fileOlderThen(stats, process.env.TTL_CONVERTED_FILES)) {
                     fs.unlink(convertedFilePath, (err) => {
                         if (err) throw err
                     })
